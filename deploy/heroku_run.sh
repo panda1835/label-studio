e29d7a53647e6a6fbac7742ad14d2e8f1a67bb54
Panda <52302733+panda1835@users.noreply.github.com>
2024-06-05 16:16:58 +0700

    Update heroku_run.sh

--- deploy/heroku_run.sh
+++ deploy/heroku_run.sh
@@ -8,7 +8,7 @@ export POSTGRE_PASSWORD="${BASH_REMATCH[2]}"
 export POSTGRE_NAME="${BASH_REMATCH[5]}"
 export POSTGRE_HOST="${BASH_REMATCH[3]}"
 export POSTGRE_PORT="${BASH_REMATCH[4]}"
-export DJANGO_DB='default'
+export DJANGO_DB='sqlite'
 
 ./deploy/docker-entrypoint.sh label-studio \
   --host ${HOST:-""} \
