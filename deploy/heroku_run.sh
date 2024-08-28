bdcc120fe443c7720f8343f21a068d7b3f11e0ea
Panda <52302733+panda1835@users.noreply.github.com>
2024-08-28 17:02:04 +0700

    Update heroku_run.sh

--- deploy/heroku_run.sh
+++ deploy/heroku_run.sh
@@ -3,18 +3,17 @@
 readonly URI_REGEX='postgres:\/\/(.+):(.+)@(.+):(.+)\/(.+)'
 
 [[ $DATABASE_URL =~ $URI_REGEX ]]
-# export POSTGRE_USER="${BASH_REMATCH[1]}"
-# export POSTGRE_PASSWORD="${BASH_REMATCH[2]}"
-# export POSTGRE_NAME="${BASH_REMATCH[5]}"
-# export POSTGRE_HOST="${BASH_REMATCH[3]}"
-# export POSTGRE_PORT="${BASH_REMATCH[4]}"
-# export DJANGO_DB='default'
-export DJANGO_DB='sqlite'
+export POSTGRE_USER="${BASH_REMATCH[1]}"
+export POSTGRE_PASSWORD="${BASH_REMATCH[2]}"
+export POSTGRE_NAME="${BASH_REMATCH[5]}"
+export POSTGRE_HOST="${BASH_REMATCH[3]}"
+export POSTGRE_PORT="${BASH_REMATCH[4]}"
+export DJANGO_DB='default'
 
 ./deploy/docker-entrypoint.sh label-studio \
   --host ${HOST:-""} \
   --port ${PORT} \
-  --username test \
-  --password test
-  # --username ${USERNAME} \
-  # --password ${PASSWORD}
+  # --username test \
+  # --password test
+  --username ${USERNAME} \
+  --password ${PASSWORD}
